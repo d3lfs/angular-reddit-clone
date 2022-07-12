0ae639bf6cd4aecbb8b595cd6dd4c30edb1fdd18
Sly Bacalso <nasa.sly14@gmail.com>
2022-07-12 16:55:59 +0800

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-reddit';
+	articles!: Array<Article>;
+
+	constructor() {
+		this.articles = new Array();
+	}
+
+	addArticle(newTitle: HTMLInputElement, newLink: HTMLInputElement) {
+		this.articles.push(new Article(newTitle.value, newLink.value, 0));
+		newTitle.value = '';
+		newLink.value = '';
+		return false;
+	}
+
+	sortedArticles(): Array<Article> {
+		return this.articles.sort(
+			(a: Article, b: Article) => b.votes - a.votes
+		);
+	}
+
+	title = 'angular-reddit';
 }
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+	selector: 'app-article',
+	templateUrl: './article.component.html',
+	styleUrls: ['./article.component.scss'],
+})
+export class ArticleComponent {
+	@HostBinding('attr.class') cssClass = 'row';
+	@Input() article!: Article;
+
+	constructor() {}
+
+	voteUp(): boolean {
+		this.article.voteUp();
+		return false;
+	}
+
+	voteDown(): boolean {
+		this.article.voteDown();
+		return false;
+	}
+}
